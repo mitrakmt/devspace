4786bc35b0402c60822788a28cf742c5d82d08bb
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-11-03 02:09:14 -0700

    [server-setup] - setup base server file structure

---
+++ server/controllers/users.controller.js
@@ -0,0 +1,23 @@
+let usersController = {}
+
+usersController.signup = () => {
+
+}
+
+usersController.login = () => {
+
+}
+
+usersController.edit = () => {
+
+}
+
+usersController.delete = () => {
+
+}
+
+usersController.logout = () => {
+
+}
+
+module.exports = usersController
