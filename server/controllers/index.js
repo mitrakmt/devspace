bb7d6c8ba73490b528b96182a4aad8d86cd4c918
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-11-03 02:09:14 -0700

    [server-setup] - setup base server file structure

---
+++ server/controllers/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  companiesController: require('./companies.controller'),
+  paymentsController: require('./payments.controller'),
+  postsController: require('./posts.controller'),
+  projectsController: require('./projects.controller'),
+  teamsController: require('./teams.controller'),
+  usersController: require('./users.controller')
+}
