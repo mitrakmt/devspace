25c081dedf3a92777adb1fb666c01c59f9196805
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-11-05 09:50:34 -0700

    [server-feature] - user routing

--- server/routers/users.router.js
+++ server/routers/users.router.js
@@ -1,19 +1,40 @@
 let usersRouter = require('express').Router()
-  // 
-  // usersRouter.route('/signup')
-  //   .post()
-  //
-  // usersRouter.route('/login')
-  //   .post()
-  //
+const passport = require('passport')
+let config = require('../config')
+
+usersRouter.route('/auth')
+  .get(passport.authenticate('github', { scope: ['user:email'] }), (req, res) => {})
+
+usersRouter.route('/auth/github/callback')
+  .get((req, res) => {
+    passport.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
+      res.redirect('/')
+    }
+  })
+
+usersRouter.route('/account')
+  .get(config.ensureAuthenticated, (req, res) => {
+    res.render('account', { user: req.user })
+  })
+
+usersRouter.route('/login')
+  .get((req, res) => {
+    res.render('login', { user: req.user })
+  })
+
+usersRouter.route('/edit')
+  .put((req, res) => {
+
+  })
+
+usersRouter.route('/logout')
+  .get((req, res) => {
+    req.logout()
+    res.redirect('/')
+  })
+
   // usersRouter.route('/delete')
   //   .delete()
-  //
-  // usersRouter.route('/edit')
-  //   .put()
-  //
-  // usersRouter.route('/logout')
-  //   .get()
   //
   // usersRouter.route('/followers')
   //   .get()
