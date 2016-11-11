fd5f5abbf37c19afbfdeee1c5dc55873042c0ccd
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-11-11 13:20:52 -0800

    [client-feature] - dynamic rendering for each user

--- client/src/app/home/newsfeed/newsfeed-list/newsfeed-list.service.ts
+++ client/src/app/home/newsfeed/newsfeed-list/newsfeed-list.service.ts
@@ -9,15 +9,17 @@ export class NewsfeedListService {
   constructor(private _http: Http) { }
 
     fetchNewsfeedUpdates(): Observable<any> {
-    let headers = new Headers({ 'userid': 1 })
-    let options = new RequestOptions({ headers: headers })
-    return this._http.get('/api/home/feed', options)
-      .map((res:Response) => res.json())
-  }
+      let userid = localStorage.getItem('userid')
+      let headers = new Headers({ 'userid': userid })
+      let options = new RequestOptions({ headers: headers })
+      return this._http.get('/api/home/feed', options)
+        .map((res:Response) => res.json())
+    }
     sendNewsfeedUpdate(post: any): Observable<any> {
+      let userid = localStorage.getItem('userid')
       let content = post._value.content;
       let body = {'content': content};
-      let headers = new Headers({'userid': 1});
+      let headers = new Headers({'userid': userid});
       headers.append('Content-Type', 'application/json');
       return this._http.post('/api/posts', body, {
         headers: headers
@@ -25,9 +27,10 @@ export class NewsfeedListService {
         .map((data) => data.json())
     }
     sendNewComment(comment: any): Observable<any> {
+      let userid = localStorage.getItem('userid')
       let content = comment._value.content;
       let body = {'content': content};
-      let headers = new Headers({'userid': 1});
+      let headers = new Headers({'userid': userid});
       headers.append('Content-Type', 'application/json');
       return this._http.post('/api/posts/comments/' + 1, body, {
         headers: headers
--- client/src/app/home/project-sidebar/project-sidebar-list/project-sidebar-list.service.ts
+++ client/src/app/home/project-sidebar/project-sidebar-list/project-sidebar-list.service.ts
@@ -9,7 +9,9 @@ export class ProjectSidebarListService {
   constructor(private _http: Http) { }
 
   getSidebarProjects(): Observable<any> {
-    let headers = new Headers({ 'userid': '1', username: 'davidkim310' })
+    let username = localStorage.getItem('username')
+    let userid = localStorage.getItem('userid')
+    let headers = new Headers({ 'userid': userid, username: username })
     let options = new RequestOptions({ headers: headers })
     return this._http.get('/api/home/sidebar', options)
       .map((res:Response) => res.json())
