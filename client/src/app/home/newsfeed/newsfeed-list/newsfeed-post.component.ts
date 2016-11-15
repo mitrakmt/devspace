6182a85af8ea7bd6a0eec48c3fbcf62cd1b43ac4
Dianne Le <diannefle@gmail.com>
2016-11-15 14:52:35 -0800

    [fix]-fix merge conflicts

--- client/src/app/home/newsfeed/newsfeed-list/newsfeed-post.component.ts
+++ client/src/app/home/newsfeed/newsfeed-list/newsfeed-post.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { NewsfeedPost } from '../newsfeed-post';
+import { NewsfeedComment } from '../newsfeed-comment'
+import { NewsfeedListService } from './newsfeed-list.service';
+
+
 @Component({
   selector: 'app-newsfeed-post',
-  templateUrl: './newsfeed-post.component.html'
+  template: `
+    <md-card style="margin-bottom: 20px" md-padding md-margin>
+      <div>
+          <h4><a href="{{newsfeedPost.user.username}}">{{newsfeedPost.user.username}}</a>: {{newsfeedPost.content}}</h4>
+          <p>{{newsfeedPost.createdAt | date:'medium'}}</p>
+          <p>likes: {{newsfeedPost.likes}}</p>
+          <app-newcomment [postId]="postId"></app-newcomment>
+      </div>
+      <div *ngIf="newsfeedPost.comments.length > 0">
+          <app-newsfeed-comments [comments]="newsfeedPost.comments"> </app-newsfeed-comments>
+      </div>
+  </md-card>
+  `
 })
 export class NewsfeedPostComponent implements OnInit {
   @Input() newsfeedPost: any;
-  postId: number;
-  constructor() { }
+  @Input('postId') postId: number;
+  constructor(private newsfeedListService: NewsfeedListService) { }
+  newsfeedComments: NewsfeedComment[] = [];
 
   ngOnInit() {
-    this.postId = this.newsfeedPost.id
+    this.postId = this.newsfeedPost.id;
     console.log("id-->", this.postId);
+    console.log("posts feed", this.newsfeedPost)
   }
 
 }
