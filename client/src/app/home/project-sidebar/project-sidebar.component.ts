c3ce54104de36d37ab1bfcf226e446d53d5ea2c4
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-11-16 23:07:14 -0800

    [client-feature] - implement import project feature

--- client/src/app/home/project-sidebar/project-sidebar.component.ts
+++ client/src/app/home/project-sidebar/project-sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectSidebarService } from './project-sidebar.service'
 import { Observable } from 'rxjs/Observable';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-project-sidebar',
@@ -9,13 +10,39 @@ import { Observable } from 'rxjs/Observable';
 })
 export class ProjectSidebarComponent implements OnInit {
 
-  constructor(private projectSidebarService: ProjectSidebarService) { 
-    // this.projectSidebarService.addProject = ():Observable<any> => {
-    //   console.log('project sidebar asdf');
-    //   let baseball = 'baseball'
-    //   return baseball
-    // }
-  }
+  project = {'name': ''}
+
+  constructor(private projectSidebarService: ProjectSidebarService) { }
+
+    onSubmit(form: NgForm) {
+        let projectName = form.value.project;
+        console.log(projectName)
+        let userId = localStorage.getItem('userid');
+        form.reset()
+        this.projectSidebarService.importProject(userId, projectName)
+          .subscribe(
+            data => {
+              return data
+              // this._newsfeedListService.newsfeedPosts.map((post)=>{
+              //   console.log("post is", post)
+              // if(post.id === this.postId){
+              //   post.comments.unshift(data)
+              // }
+              // return post
+              // })
+      })
+    }
+
+  // importProject = () => {
+  //   let userId = localStorage.getItem('userid');
+  //   let projectName = 'blah';
+  //   this.projectSidebarService.importProject(userId, projectName)
+  //     .subscribe(
+  //       data => {
+  //         return data
+  //       }
+  //     )
+  // }
 
   ngOnInit() {
     
