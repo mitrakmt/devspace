92060bfec60f558f0e7ff98d165adc982721eef2
davidkim310 <davidkim310@gmail.com>
2016-11-21 15:47:13 -0800

    [client-feat]-dynamic rendering of projects list

--- client/src/app/home/project-sidebar/project-sidebar.component.ts
+++ client/src/app/home/project-sidebar/project-sidebar.component.ts
@@ -7,8 +7,17 @@ import { Project } from './project';
 
 @Component({
   selector: 'app-project-sidebar',
-  templateUrl: './project-sidebar.component.html',
-  styleUrls: ['./project-sidebar.component.css']
+  styleUrls: ['./project-sidebar.component.css'],
+  template: `
+        <app-project-sidebar-list></app-project-sidebar-list>
+        <div class="dropdown">
+          <button class="dropbtn">Import Github Project</button>
+          <div class="dropdown-content">
+            <md-input class="add-project-input" placeholder="Import project" type="text" [value]="project" (input)="project = $event.target.value"></md-input>
+            <button md-button md-raised (click)="importProject(project)" class="main-button">Import</button>
+          </div>
+        </div>
+  `
 })
 export class ProjectSidebarComponent implements OnInit {
   sidebarProjects: Project[] = [];
@@ -20,9 +29,9 @@ export class ProjectSidebarComponent implements OnInit {
         this.projectSidebarService.importProject(userId, projectName)
           .subscribe(
             data => {
-              this.projectSidebarListService.sidebarProjects.push(data.name)
-              this.sidebarProjects.push(data.name)
-      })
+              this.projectSidebarListService.sidebarProjects.unshift(data)
+              this.sidebarProjects.unshift(data)
+          })
     }
 
   ngOnInit() {
