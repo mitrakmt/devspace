9b7e9f944e44e7b0d466e4b2ef475cde6229b85f
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-11-27 19:52:24 -0800

    [fix] - fix heroku bugs

--- client/protractor.conf.js
+++ client/protractor.conf.js
@@ -13,7 +13,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'https://devspace.herokuapp.com/',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
